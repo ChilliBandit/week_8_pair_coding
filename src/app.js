9c5aa26bd6087e6ec0bbfef65511f9104ff7f452
EmoryC <emory-crane@live.nmit.ac.nz>
2022-04-06 09:25:27 +1200

    function

---
+++ src/app.js
@@ -0,0 +1,7 @@
+//create a function that takes an array containing only numbers and return the first element
+// example: getarrelement([1, 2, 3, 4]) ->1
+//SECTION Creating a function
+
+function getarrelement([arr]) {
+     return arr[0]
+}
