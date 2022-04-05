84651e1d8516d6b7eab485379f187dbc9bf56a21
EmoryC <emory-crane@live.nmit.ac.nz>
2022-04-06 11:20:18 +1200

    pop

--- src/app.js
+++ src/app.js
@@ -23,14 +23,6 @@ console.log(createarray(1, 2))
 //Write a function to reverse an array 
 //Example: reverseArray([1, 2, 3, 4]) -> [4, 3, 2, 1]
 
-// function reverseArray([a, b, c, d]) {
-//     var array2 = [d, c, b, a]
-//     return array2
-// }
-// console.log(reverseArray([1, 2, 3, 4, 5]))
-//NOTE Providing parameters for the function, then a variable with the reversed parameters
-    // We then ask the function to return the variable within the initial parameters provided
-
 // function reversearray(x) {
 //     return x.reverse()
 // }
@@ -38,3 +30,14 @@ console.log(createarray(1, 2))
 
 const reversearray = x => x.reverse()
 console.log(reversearray([1, 2, 3, 4, 10]))
+//NOTE Arrow function^^^
+
+//create a function that accepts an array and return the last element
+//Example: lastElement(["EC", 3, "left", "Phoenix"])
+
+function lastelement(array3) {
+        return array3.pop()
+}
+console.log(lastelement(["Ec", 2, "twenty", 12, "banana"]))
+//arr.length -1
+//pop()
