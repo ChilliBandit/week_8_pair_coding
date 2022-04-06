90d5015d953c3bb53c6416ff14c47b9c7c30033e
hh hh <phoenix-binniegenet@live.nmit.ac.nz>
2022-04-06 14:40:23 +1200

    m

--- src/app.js
+++ src/app.js
@@ -143,3 +143,20 @@
  //ES6
 
  const objToArr = obj => Object.entries(obj)
+
+
+ // Create a function that takes an array of arrays and turns a single new array with largest number of each
+// example: findMaxnumber([[1,2,3], [4,5,6], [7,8,9]]) -> [3,6,9]
+ function findMaxNumber(arr) {
+     let newArr = []
+     for(let i = 0; i < arr.length; i++) {
+         let maxNumber = arr[i][0]
+         for(let j = 0; j < arr[i].length; j++){
+          if(arr[i][j]> maxNumber) {
+          maxNumber = arr[i][j]
+          }
+         }
+      newArr.push(maxNumber)
+     }
+     return newArr
+ }
